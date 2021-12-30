ed12cf4c4a82a3331d899443332f75353fed6d77
atelier4 <sanaeelmadani3@gmail.com>
2021-12-30 04:06:32 -0600

    first commit

---
+++ helloworld/src/helloworld/hello.java
@@ -0,0 +1,19 @@
+package helloworld;
+
+public class hello {
+	
+	// Votre programme commence par main(). 
+
+	// Écrire "Hello, World" dans le terminal de window. 
+
+	public static void main(String args[]) 
+
+	{ 
+
+	System.out.println("Hello, World"); 
+
+	} 
+
+	}
+
+
